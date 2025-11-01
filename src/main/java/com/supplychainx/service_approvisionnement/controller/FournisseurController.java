141e7cef509865ca54b0c91f6fab7c283530e716
Mouad Hallaffou <mouadhallaffou@gmail.com>
2025-11-01 22:27:10 +0100

    add FournisseurController and fournisseur.http for supplier management API

---
+++ src/main/java/com/supplychainx/service_approvisionnement/controller/FournisseurController.java
@@ -0,0 +1,47 @@
+package com.supplychainx.service_approvisionnement.controller;
+
+import com.supplychainx.handler.GlobalSuccessHandler;
+import com.supplychainx.service_approvisionnement.dto.FournisseurRequestDTO;
+import com.supplychainx.service_approvisionnement.dto.FournisseurResponseDTO;
+import com.supplychainx.service_approvisionnement.service.FournisseurService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/fournisseurs")
+@RequiredArgsConstructor
+public class FournisseurController {
+    private final FournisseurService fournisseurService;
+
+    @GetMapping
+    public List<FournisseurResponseDTO> getAllFournisseurs() {
+        return fournisseurService.getAll();
+    }
+
+    @PostMapping
+    public FournisseurResponseDTO createFournisseur(@Valid @RequestBody FournisseurRequestDTO fournisseurDTO) {
+        return fournisseurService.create(fournisseurDTO);
+    }
+
+    @GetMapping("/{id}")
+    public FournisseurResponseDTO getFournisseurById(@PathVariable("id") Long id) {
+        return fournisseurService.getById(id);
+    }
+
+    @PutMapping("/{id}")
+    public FournisseurResponseDTO updateFournisseur(@PathVariable("id") Long id,
+                                                    @Valid @RequestBody FournisseurRequestDTO fournisseurDTO) {
+        return fournisseurService.update(fournisseurDTO, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Map<String, Object>> deleteFournisseur(@PathVariable("id") Long id) {
+        fournisseurService.delete(id);
+        return GlobalSuccessHandler.handleDeleted("Fournisseur deleted successfully");
+    }
+}
