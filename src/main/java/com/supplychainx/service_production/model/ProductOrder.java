3c58a344e6c2895dbd958b679f8ec594abbac3b3
Mouad Hallaffou <mouadhallaffou@gmail.com>
2025-11-04 21:05:12 +0100

    add ProductOrder model, controller, and mapper for managing product orders

---
+++ src/main/java/com/supplychainx/service_production/model/ProductOrder.java
@@ -0,0 +1,36 @@
+package com.supplychainx.service_production.model;
+
+import com.supplychainx.service_production.model.enums.ProductionOrderStatus;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "product_orders")
+public class ProductOrder {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "product_order_id", nullable = false)
+    private Long productOrderId;
+
+    @Column(name = "quantity", nullable = false)
+    private Integer quantity;
+
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private ProductionOrderStatus status;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDateTime startDate;
+
+    @Column(name = "end_date", nullable = true)
+    private LocalDateTime endDate;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+}
