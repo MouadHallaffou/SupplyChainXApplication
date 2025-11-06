a8ec718292684a501f6aefd77e983a9c20ea170b
Mouad Hallaffou <mouadhallaffou@gmail.com>
2025-11-06 15:19:45 +0100

    refactor Product model and related classes for improved structure and relationships

--- src/main/java/com/supplychainx/service_production/dto/ProductResponseDTO.java
+++ src/main/java/com/supplychainx/service_production/dto/ProductResponseDTO.java
@@ -1,10 +1,7 @@
 package com.supplychainx.service_production.dto;
 
-import lombok.Builder;
-
 import java.time.LocalDateTime;
 
-@Builder
 public record ProductResponseDTO(
         Long productId, String name,
         Integer productionTimeHours,
