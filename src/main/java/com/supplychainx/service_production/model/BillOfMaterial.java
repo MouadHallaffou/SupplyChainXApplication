d10a20f9235ec34e0c10d5dbd6839fa5a9d60309
Mouad Hallaffou <mouadhallaffou@gmail.com>
2025-11-06 15:23:00 +0100

    refactor BillOfMaterial to extend BaseEntity and remove timestamp fields; update CommandeFournisseurController to validate request body

--- src/main/java/com/supplychainx/service_production/model/BillOfMaterial.java
+++ src/main/java/com/supplychainx/service_production/model/BillOfMaterial.java
@@ -1,18 +1,20 @@
 package com.supplychainx.service_production.model;
 
 import com.supplychainx.service_approvisionnement.model.MatierePremiere;
+import com.supplychainx.util.BaseEntity;
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
 
-import java.time.LocalDateTime;
-
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "bill_of_materials")
-public class BillOfMaterial {
+public class BillOfMaterial extends BaseEntity {
 
     @Id
-    @GeneratedValue( strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "bom_id", nullable = false)
     private Long bomId;
 
@@ -26,22 +28,5 @@ public class BillOfMaterial {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "matiere_premiere_id", nullable = false)
     private MatierePremiere matierePremiere;
-
-    @Column(name = "created_at", nullable = false, updatable = false)
-    private LocalDateTime createdAt;
-
-    @Column(name = "updated_at", nullable = false)
-    private LocalDateTime updatedAt;
-
-    @PrePersist
-    private void OnCreate() {
-        LocalDateTime now = LocalDateTime.now();
-        this.createdAt = now;
-        this.updatedAt = now;
-    }
-    @PreUpdate
-    public void OnUpdate() {
-        this.updatedAt = LocalDateTime.now();
-    }
 
 }
