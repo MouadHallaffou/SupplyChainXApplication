85e6694cd44056ee4056487da8fa0972fe6511ad
Mouad Hallaffou <mouadhallaffou@gmail.com>
2025-11-08 19:58:59 +0100

    add Address queries and mutations for GraphQL; refactor AddressMapper and update AddressRequestDTO

--- src/main/java/com/supplychainx/service_livraison/dto/Request/AddressRequestDTO.java
+++ src/main/java/com/supplychainx/service_livraison/dto/Request/AddressRequestDTO.java
@@ -1,17 +1,18 @@
-package com.supplychainx.service_livraison.dto;
+package com.supplychainx.service_livraison.dto.Request;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class AddressRequestDTO {
-    private Long addressId;
     @NotBlank(message = "Street is required")
     private String street;
     @NotBlank(message = "City is required")
     private String city;
     private String state;
+    private String country;
     @NotBlank(message = "Zip code is required")
     private String zipCode;
+    @NotBlank(message = "Client ID is required")
     private Long clientId;
 }
