7f9b9af25d4022d6cada9362c58cd11bbf55cd7d
Mouad Hallaffou <mouadhallaffou@gmail.com>
2025-11-08 20:11:24 +0100

    refactor RoleController add authaurisation (ckeck email, password , role)

--- src/main/java/com/supplychainx/service_user/controller/RoleController.java
+++ src/main/java/com/supplychainx/service_user/controller/RoleController.java
@@ -21,47 +21,33 @@ public class RoleController {
     private final AuthUtil authUtil;
 
     @GetMapping
-    public List<RoleResponseDTO> getAllRoles(
-            @RequestHeader("email") String email,
-            @RequestHeader("password") String password) {
+    public List<RoleResponseDTO> getAllRoles(@RequestHeader("email") String email, @RequestHeader("password") String password) {
         authUtil.verifyAccess(email, password, "ADMIN");
         return roleService.getAll();
     }
 
     @PostMapping
-    public ResponseEntity<Map<String, Object>> createRole(
-            @RequestHeader("email") String email,
-            @RequestHeader("password") String password,
-            @Valid @RequestBody RoleRequestDTO roleRequestDTO) {
+    public ResponseEntity<Map<String, Object>> createRole(@RequestHeader("email") String email, @RequestHeader("password") String password, @Valid @RequestBody RoleRequestDTO roleRequestDTO) {
         authUtil.verifyAccess(email, password, "ADMIN");
         RoleResponseDTO createdDTO = roleService.create(roleRequestDTO);
         return GlobalSuccessHandler.handleSuccessWithData("Role created successfully", createdDTO);
     }
 
     @GetMapping("/{id}")
-    public RoleResponseDTO getRoleById(
-            @RequestHeader("email") String email,
-            @RequestHeader("password") String password,
-            @PathVariable("id") Long id) {
+    public RoleResponseDTO getRoleById(@RequestHeader("email") String email, @RequestHeader("password") String password, @PathVariable("id") Long id) {
         authUtil.verifyAccess(email, password, "ADMIN");
         return roleService.getById(id);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Map<String, Object>> updateRole(
-            @RequestHeader("email") String email,
-            @RequestHeader("password") String password,
-            @PathVariable("id") Long id, @Valid @RequestBody RoleRequestDTO roleRequestDTO) {
+    public ResponseEntity<Map<String, Object>> updateRole(@RequestHeader("email") String email, @RequestHeader("password") String password, @PathVariable("id") Long id, @Valid @RequestBody RoleRequestDTO roleRequestDTO) {
         authUtil.verifyAccess(email, password, "ADMIN");
         RoleResponseDTO updatedDTO = roleService.update(id, roleRequestDTO);
         return GlobalSuccessHandler.handleSuccessWithData("Role updated successfully", updatedDTO);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Map<String, Object>> deleteRole(
-            @RequestHeader("email") String email,
-            @RequestHeader("password") String password,
-            @PathVariable("id") Long id) {
+    public ResponseEntity<Map<String, Object>> deleteRole(@RequestHeader("email") String email, @RequestHeader("password") String password, @PathVariable("id") Long id) {
         authUtil.verifyAccess(email, password, "ADMIN");
         roleService.delete(id);
         return GlobalSuccessHandler.handleDeleted("Role deleted successfully");
